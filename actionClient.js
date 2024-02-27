00a0b0f973b9e87db16f36ec6f33f5470ec9fcff
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-02-27 10:57:40 -0500

    Refactor code in pieces

---
+++ actionClient.js
@@ -0,0 +1,50 @@
+// actionClient.js
+
+export default {
+  props: ['ros', 'connected'],
+  data() {
+    return {
+      goal: null,
+      action: {
+        goal: { position: { x: 0, y: 0, z: 0 } },
+        feedback: { position: 0, state: 'idle' },
+        result: { success: false },
+        status: { status: 0, text: '' },
+      },
+    };
+  },
+  methods: {
+    sendGoal: function() {
+            console.log('sendGoal called');
+            let actionClient = new ROSLIB.ActionClient({
+                ros : this.ros,
+                serverName : '/turtlebot2_action_service_as',
+                actionName : 'course_web_dev_ros/WaypointActionAction'
+            })
+
+            this.goal = new ROSLIB.Goal({
+                actionClient : actionClient,
+                goalMessage: {
+                    ...this.action.goal
+                }
+            })
+
+            this.goal.on('status', (status) => {
+                this.action.status = status
+            })
+
+            this.goal.on('feedback', (feedback) => {
+                this.action.feedback = feedback
+            })
+
+            this.goal.on('result', (result) => {
+                this.action.result = result
+            })
+
+            this.goal.send()
+        },
+        cancelGoal: function() {
+            this.goal.cancel()
+        },
+  },
+};
---
+++ rosConnection.js
@@ -0,0 +1,38 @@
+// rosConnection.js
+
+export default {
+  data() {
+    return {
+      ros: null,
+      connected: false,
+      loading: false,
+      logs: [],
+      rosbridge_address: 'wss',
+    };
+  },
+  methods: {
+    connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+            this.goal = null
+        },
+  },
+};
