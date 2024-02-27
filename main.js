5bef43ddcaa63947080d6f8ecbe4fef66b930aab
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-02-27 15:39:04 -0500

    Add camera module to main.js

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
 import rosConnection from './rosConnection.js';
 import actionClient from './actionClient.js';
 import visualizeModel from './visualizeModel.js';
+import camera from './camera.js';
 
 var app = new Vue({
   el: '#app',
-  mixins: [rosConnection, actionClient, visualizeModel],
+  mixins: [rosConnection, actionClient, visualizeModel, camera],
   mounted() {
     // Your code here...
   },
