58432b304f2e5a2c724815b867478d7fab11d6fd
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-02-27 12:19:10 -0500

    Add waypoints and update goal selection

--- actionClient.js
+++ actionClient.js
@@ -4,6 +4,14 @@ export default {
   props: ['ros', 'connected'],
   data() {
     return {
+      waypoints: [
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 },
+        { x: 3, y: 3 },
+        { x: 4, y: 4 },
+      ],
+      selectedWaypoint: null,
       goal: null,
       action: {
         goal: { position: { x: 0, y: 0, z: 0 } },
@@ -14,37 +22,41 @@ export default {
     };
   },
   methods: {
-    sendGoal: function() {
-            console.log('sendGoal called');
-            let actionClient = new ROSLIB.ActionClient({
-                ros : this.ros,
-                serverName : '/tortoisebot_as',
-                actionName : 'course_web_dev_ros/WaypointActionAction'
-            })
+    sendGoal: function () {
+      console.log('sendGoal called');
+      let actionClient = new ROSLIB.ActionClient({
+        ros: this.ros,
+        serverName: '/tortoisebot_as',
+        actionName: 'course_web_dev_ros/WaypointActionAction'
+      })
 
-            this.goal = new ROSLIB.Goal({
-                actionClient : actionClient,
-                goalMessage: {
-                    ...this.action.goal
-                }
-            })
+      this.goal = new ROSLIB.Goal({
+        actionClient: actionClient,
+        goalMessage: {
+          ...this.action.goal
+        }
+      })
 
-            this.goal.on('status', (status) => {
-                this.action.status = status
-            })
+      this.goal.on('status', (status) => {
+        this.action.status = status
+      })
 
-            this.goal.on('feedback', (feedback) => {
-                this.action.feedback = feedback
-            })
+      this.goal.on('feedback', (feedback) => {
+        this.action.feedback = feedback
+      })
 
-            this.goal.on('result', (result) => {
-                this.action.result = result
-            })
+      this.goal.on('result', (result) => {
+        this.action.result = result
+      })
 
-            this.goal.send()
-        },
-        cancelGoal: function() {
-            this.goal.cancel()
-        },
+      this.goal.send()
+    },
+    cancelGoal: function () {
+      this.goal.cancel()
+    },
+    updateGoal() {
+      this.action.goal.position.x = this.selectedWaypoint.x;
+      this.action.goal.position.y = this.selectedWaypoint.y;
+    },
   },
 };
--- rosConnection.js
+++ rosConnection.js
@@ -11,28 +11,28 @@ export default {
     };
   },
   methods: {
-    connect: function() {
-            this.loading = true
-            this.ros = new ROSLIB.Ros({
-                url: this.rosbridge_address
-            })
-            this.ros.on('connection', () => {
-                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
-                this.connected = true
-                this.loading = false
-            })
-            this.ros.on('error', (error) => {
-                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
-            })
-            this.ros.on('close', () => {
-                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
-                this.connected = false
-                this.loading = false
-            })
-        },
-        disconnect: function() {
-            this.ros.close()
-            this.goal = null
-        },
+    connect: function () {
+      this.loading = true
+      this.ros = new ROSLIB.Ros({
+        url: this.rosbridge_address
+      })
+      this.ros.on('connection', () => {
+        this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+        this.connected = true
+        this.loading = false
+      })
+      this.ros.on('error', (error) => {
+        this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+      })
+      this.ros.on('close', () => {
+        this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+        this.connected = false
+        this.loading = false
+      })
+    },
+    disconnect: function () {
+      this.ros.close()
+      this.goal = null
+    },
   },
 };
