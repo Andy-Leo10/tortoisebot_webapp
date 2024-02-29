fa03c22a8770b9f585541ca0f3da811e05a4f237
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-02-29 11:57:27 -0500

    Update camera stream dimensions and rearrange UI elements

--- camera.js
+++ camera.js
@@ -10,8 +10,8 @@ export default {
             console.log(domain)
 
             let topic = '/camera/image_raw';
-            let width = 420;
-            let height = 360;
+            let width = 320;
+            let height = 240;
 
             let host = domain + '/cameras/stream?topic=' + topic + '&width=' + width + '&height=' + height;
             console.log('Stream URL-X:', host);
@@ -19,8 +19,8 @@ export default {
             let viewer = new MJPEGCANVAS.Viewer({
                 divID: 'divCamera',
                 host: host,
-                width: 420,
-                height: 360,
+                width: width,
+                height: height,
                 topic: '/camera/image_raw',
                 ssl: true,
             })
